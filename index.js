9e8a0e5a38cb565f58c137741e1fdcd32468bd33
Timothy Lyle <itsfreezingouthere@yahoo.com>
2022-01-01 17:29:40 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+const employee = {
+    name: "tim",
+    streetAddress: 1295
+};
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const newObj = { ...obj };
+    newObj[key] = value;
+    return newObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+
+    return obj;
+};
+
+function deleteFromEmployeeByKey(obj, key) {
+    const newObj = { ...obj };
+    delete newObj[key]
+    return newObj
+};
+
+function destructivelyDeleteFromEmployeeByKey(employeeObject, key) {
+    
+    delete employeeObject[key]
+    return employeeObject
+};
+
+
+
